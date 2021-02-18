7b0991b0083e9a8688d330c2f5f287c3d118e8f1
Jainish Patel <jainishpatel10597@gmail.com>
2021-02-18 17:51:46 -0500

    Remove Home1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Employee from './Employee';
+import Header1 from './Header1';
+import AddEmployeeForm from './AddEmployeeForm';
+const Home = () =>  {
+  const [employee, setEmployee] = useState ([
+    {
+      name: '',
+      id: '',
+    },
 
-const Home = (props) => {
-    return (
-      <header>
-        <h1>Home</h1>
-             
-      </header>
-    );
+  ]);
+
+  let previd = 0;
+  const handleAddEmployee = (name) => {
+    setEmployee([
+      ...employee,
+      {
+        name: name, 
+        id: (previd += 1),
+      },
+    ]);
   };
-  
-  export default Home;
-  
+
+  return (
+    <div>
+        
+      <Header title='Employee Management System'/> 
+
+     <Header1 title='Employee List' totalEmployees={employee.length-1}/>
+
+      {employee.map((g) => (
+     <Employee  name={g.name} id={g.id} key={g.id.toString()} />     
+      ))}
+      <AddEmployeeForm addEmployee={handleAddEmployee} />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
