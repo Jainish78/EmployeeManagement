0e7f7cabfd4cc67e6e9584e31e741b1532f943a1
Prima Patel <primapatel.811@gmail.com>
2021-02-17 21:04:14 -0500

    add home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = (props) => {
+    return (
+      <header>
+        <h1>Home</h1>
+             
+      </header>
+    );
+  };
+  
+  export default Home;
+  
