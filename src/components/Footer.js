38855fd501ffa0760ecb3767480f8f793739c9a7
Jainish Patel <jainishpatel10597@gmail.com>
2021-02-17 20:52:30 -0500

    Initial

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+      <header>
+        <h1>Footer</h1>
+             
+      </header>
+    );
+  };
+  
+  export default Footer;
+  
